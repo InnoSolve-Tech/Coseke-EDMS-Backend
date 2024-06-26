06bc79df1da3514867d34ec3a895f8df8e4236c0
BenziDarwin <ssalibenjamin0402@gmail.com>
2024-06-26 12:20:36 +0300

    Added Directory view

--- src/main/java/com/cosek/edms/directory/DirectoryController.java
+++ src/main/java/com/cosek/edms/directory/DirectoryController.java
@@ -2,6 +2,7 @@ package com.cosek.edms.directory;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/directories")
 @CrossOrigin(origins = "http://localhost:3000")
 public class DirectoryController {
 
@@ -24,40 +25,40 @@ public class DirectoryController {
     @Autowired
     private DirectoryService directoryService;
 
-    @GetMapping("/directories")
+    @GetMapping("/")
     public List<Directory> getAllDirectories() {
         return directoryService.getAllDirectories();
     }
 
-    @GetMapping("/directories/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Directory> getDirectoryById(@PathVariable(value = "id") Long directoryId)
             throws Exception {
         Directory directory = directoryService.getDirectoryById(directoryId);
         return ResponseEntity.ok().body(directory);
     }
 
-    @PostMapping("/directories")
+    @PostMapping("/")
     public Directory createDirectory(@RequestBody Directory directory) {
         System.out.println("Created Directory: " + directory);
         return directoryService.createDirectory(directory);
     }
     
-    @PostMapping("/directories/create")
+    @PostMapping("/create")
     public Directory createDirectoryWithName(@RequestBody Directory directory) {
        return directoryService.creaDirectoryWithName(directory);
     }
 
-    @PostMapping("/directories/subfolder")
+    @PostMapping("/subfolder")
     public Directory createSubFolder(@RequestBody Directory directory) {
       return directoryService.createSubDirectory(directory);
     }
 
-    @GetMapping("/directories/last")
+    @GetMapping("/last")
     public List<Directory> searchDocuments() {
         return directoryRepository.findByLastInput();
     }
 
-    @PutMapping("/directories/{folderID}")
+    @PutMapping("/{folderID}")
     public Directory updateFolderParent(@PathVariable Long folderID, @RequestBody Directory directoryRequest) {
         return directoryService.updateDirectoryParent(folderID, directoryRequest);
     }
