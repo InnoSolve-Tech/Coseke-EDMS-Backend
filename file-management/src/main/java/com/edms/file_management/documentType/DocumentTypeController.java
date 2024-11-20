1c6823ad297b473b2d5577b2abd537215d0082bf
Benzi Darwin <ssalibenjamin0402@gmail.com>
2024-11-20 12:40:33 +0300

    Added some changes

--- file-management/src/main/java/com/edms/file_management/documentType/DocumentTypeController.java
+++ file-management/src/main/java/com/edms/file_management/documentType/DocumentTypeController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/document-types")
-@CrossOrigin(origins = "http://localhost:3000")
 public class DocumentTypeController {
 
     @Autowired
