34ad9b7234050c3655d1f88eea395cb1010fc242
SSEMWOGERERE948 <strevor948@gmail.com>
2024-11-21 16:14:31 +0300

    removed cors in filemanagement

--- file-management/src/main/java/com/edms/file_management/filemanager/FileManagerController.java
+++ file-management/src/main/java/com/edms/file_management/filemanager/FileManagerController.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/files")
-@CrossOrigin("*")
 public class FileManagerController {
 
     private final FileManagerService fileService;
