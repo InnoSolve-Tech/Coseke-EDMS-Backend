d835db0f9323658626ed80485211a7e3390f4cd2
SSEMWOGERERE948 <strevor948@gmail.com>
2024-12-05 10:14:32 +0300

    trying to fix directories

--- file-management/src/main/java/com/edms/file_management/directory/DirectoryController.java
+++ file-management/src/main/java/com/edms/file_management/directory/DirectoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/directories")
+@RequestMapping("/api/v1/folders")
 public class DirectoryController {
 
     @Autowired
