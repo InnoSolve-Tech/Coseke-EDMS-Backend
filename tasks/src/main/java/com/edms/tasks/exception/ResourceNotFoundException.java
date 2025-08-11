629462b2b735a2f4461b3bdbf9eaee458ec7f76e
Ssali Benjamin <85361030+BenziDarwin@users.noreply.github.com>
2025-08-11 11:15:46 +0300

    removed tasks

--- tasks/src/main/java/com/edms/tasks/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.edms.tasks.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-    
-}
